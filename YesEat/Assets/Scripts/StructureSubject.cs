156cdc48777c9b6b5531c7e62930ccfd8360b6a0
Jess <jkperrenoud@gmail.com>
2017-07-16 18:31:29 -0700

    Item, Structure, Food Subjects
    and a shell for BuildRecipes

---
+++ YesEat/Assets/Scripts/StructureSubject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Structures are built
+/// </summary>
+
+public class StructureSubject : Subject
+{
+	#region Private members
+	private BuildRecipe buildDirections;
+	#endregion
+
+	public StructureSubject () : base()
+	{
+		name = new String("Structure");
+		description = new String("A Structure that is Built");
+		icon = new Sprite();
+
+		buildDirections = new BuildRecipe ();
+	}
+
+	/// <summary>
+	/// The BuildRecipe used to make this Structure
+	/// </summary>
+	public BuildRecipe BuildDirections
+	{
+		get { return buildDirections; }
+		set { buildDirections = value; }
+	}
+
+
+}
+
