1e0298b2f3550ad0ce0d98c3c5720f90efa4f0ab
Jess <jkperrenoud@gmail.com>
2017-07-19 22:52:54 -0700

    Very Little added to Inventory Item
    Hopefully working properly with recent change in MasterSubjectList

--- YesEat/Assets/Scripts/InventoryItem.cs
+++ YesEat/Assets/Scripts/InventoryItem.cs
@@ -41,22 +41,66 @@ public class InventoryItem
         set { stackSize = value; }
     }
 
-    public int CombineItems(int id, int number, MasterSubjectList subList)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="newSubjectID"></param>
+    /// <param name="newStackSize"></param>
+    /// <returns></returns>
+    public bool SetStack (int newSubjectID, int newStackSize)
     {
-        if (id == subjectID) stackSize += number;
-        else return number; //not the same Item
+        ItemSubject newSubject = masterSubjectList.GetSubject(newSubjectID, ItemSubject) as ItemSubject;
 
-        ItemSubject tempSubject = subList.GetSubject(subjectID) as ItemSubject;
-        int max = tempSubject.MaxStack;
+        if (newSubject != null)
+        {
+            subjectID = newSubjectID;
+            stackSize = Math.Min(newStackSize, newSubject.MaxStack);
+            return true;
+        }
+        else return false;
+    }
 
-        if (stackSize > max) return stackSize - max;
-        else return 0;
+    /// <summary>
+    /// AddStack will return the number remaining that would not fit based on ItemSubject.maxStack
+    /// </summary>
+    /// <param name="newSubjectID"></param>
+    /// <param name="newStackSize"></param>
+    /// <returns></returns>
+    public int AddStack(int newSubjectID, int newStackSize)
+    {
+        if (newSubjectID != subjectID) return newStackSize; //reject entire amount
+
+        ItemSubject newSubject = masterSubjectList.GetSubject(newSubjectID, ItemSubject) as ItemSubject;
+
+        if (newSubject != null)
+        {
+            int tempStack = stackSize + newStackSize;
+            stackSize = Math.Min(tempStack, newSubject.MaxStack);
+            newStackSize = Math.Max(0, tempStack - newSubject.MaxStack);
+        }
+        return newStackSize;
 
     }
 
-    public Subject GetSubject()
+    /// <summary>
+    /// Override for AddStack takes an Inventory Item to add to this Inventory Item and returns amount that would not fit
+    /// </summary>
+    /// <param name="addedInvItem"></param>
+    /// <returns></returns>
+    public int AddStack(InventoryItem addedInvItem)
     {
-        return masterSubjectList.GetSubject(subjectID);
+        if (addedInvItem.subjectID != subjectID) return addedInvItem.stackSize; //reject entire amount
+
+        ItemSubject newSubject = masterSubjectList.GetSubject(addedInvItem.subjectID, ItemSubject) as ItemSubject;
+
+        if (newSubject != null)
+        {
+            int tempStack = stackSize + addedInvItem.stackSize;
+            stackSize = Math.Min(tempStack, newSubject.MaxStack);
+            addedInvItem.stackSize = Math.Max(0, tempStack - newSubject.MaxStack);
+        }
+        return addedInvItem.stackSize;
+
     }
 
 }
